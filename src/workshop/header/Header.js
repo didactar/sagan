fd43e1770da3688883d1bd67d8f7ef6f8226ae40
jorgeaber <jorgeaber@gmail.com>
2017-02-08 22:45:48 -0300

    Edit style of Workshop NavBar and Header

--- src/workshop/header/Header.js
+++ src/workshop/header/Header.js
@@ -1,44 +1,57 @@
 import React, { PropTypes } from 'react'
 
 
-const TabButton = ({activeTab, tab, icon, onClick}) =>
-    <i 
-        onClick={onClick} 
-        className={
-            activeTab === tab 
-                ? 'workshop-navbar-icon-active fa ' + icon
-                : 'workshop-navbar-icon fa ' + icon 
-        }
-    ></i>
+const TabButton = ({activeTab, tab, icon, onClick, label}) =>
+    <span 
+        onClick={onClick}
+        className={activeTab === tab 
+            ? 'workshop-navbar-tab-active' 
+            : 'workshop-navbar-tab' }
+    >
+        <i className={'fa ' + icon}></i>
+        <span className='workshop-navbar-label'>
+            {label}
+        </span>
+    </span>
 
 
 const Navbar = props =>
     <div className='workshop-navbar'>
-        <div className='workshop-navbar-tab'>
+        <div className='workshop-navbar-tabs'>
             <TabButton 
                 tab='guide'
                 activeTab={props.activeTab} 
-                icon='fa-map-signs' 
+                icon='fa-map-o' 
                 onClick={props.showGuide} 
+                label='guide'
             />
             <TabButton 
                 tab='calendar'
                 activeTab={props.activeTab} 
                 icon='fa-calendar-o' 
                 onClick={props.showCalendar} 
+                label='calendar'
             />
             <TabButton 
                 tab='community'
                 activeTab={props.activeTab} 
                 icon='fa-group' 
                 onClick={props.showCommunity} 
+                label='community'
             />
             <TabButton 
-                tab='like'
+                tab='related'
                 activeTab={props.activeTab} 
-                icon='fa-heart-o' 
+                icon='fa-hashtag' 
+                label='related'
             />
         </div>
+    </div>
+
+
+const Bookmark = () =>
+    <div className='workshop-header-bookmark'>
+        Bookmark
     </div>
 
 
@@ -48,15 +61,20 @@ const WorkshopName = ({name}) =>
     </div>
 
 
+const HeaderContent = props =>
+    <div className='workshop-header-content'>
+        <WorkshopName name={props.workshop.name} />
+        <Bookmark workshop={props.workshop} />
+        <Navbar {...props} />
+    </div>
+
+
 const Header = props =>
     <div 
         className='workshop-header'
         style={{'backgroundImage': 'url(/images/' + props.workshop.image + '.jpg)'}}
     >
-        <div className='workshop-header-degrade'>
-            <WorkshopName name={props.workshop.name} />
-            <Navbar {...props} />
-        </div>
+        <HeaderContent {...props} />
     </div>
 
 
