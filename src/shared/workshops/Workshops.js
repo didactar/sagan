7434d5ef8f1c44bcfaf143d9e7e15c37915b4f8c
Jorge Berrueta <jorgeaber@gmail.com>
2017-04-19 02:34:57 -0300

    Simplify workshop item in workshop list

--- src/shared/workshops/Workshops.js
+++ src/shared/workshops/Workshops.js
@@ -2,25 +2,16 @@ import React, {PropTypes} from 'react'
 import { Spinner } from '../../shared'
 
 
-const WorkshopText = ({workshop}) =>
-    <div className='workshop-item-text'>
-        <div className='workshop-item-text-content'>
-            <div className='workshop-item-text-name'>
-                {workshop.name}
-            </div>
-            <div className='workshop-item-text-next'>
-                20 February 2017
-            </div>
-        </div>
-    </div>
-
-
 const Workshop = ({workshop}) =>
     <a
         className='workshop-item'
         href={'/workshop/' + workshop.slug }
     >
-        <WorkshopText workshop={workshop} />
+        <div className='workshop-item-degrade'>
+            <div className='workshop-item-name'>
+                {workshop.name}
+            </div>
+        </div>
     </a>
 
 
