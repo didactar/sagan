ded1ac9c493b7f7cf32d2170dff339083054595f
Jorge Berrueta <jorgeaber@gmail.com>
2017-04-20 16:46:02 -0300

    Add content to resources filter navbar

--- src/workshop/subheader/community/Community.js
+++ src/workshop/subheader/community/Community.js
@@ -1,7 +1,7 @@
 import React  from 'react'
 
 
-const CommunityTab = ({name, number}) =>
+const Tab = ({name, number}) =>
     <div className='workshop-community-tab'>
         <span className='workshop-community-tab-number'>
             {number}
@@ -14,10 +14,10 @@ const CommunityTab = ({name, number}) =>
 
 const Community = props =>
     <div className='workshop-community'>
-        <CommunityTab name='community' number='202k' />
-        <CommunityTab name='guides' number='151k' />
-        <CommunityTab name='moderators' number='14' />
-        <CommunityTab name='learners' number='121k' />
+        <Tab name='community' number='202k' />
+        <Tab name='guides' number='151k' />
+        <Tab name='moderators' number='14' />
+        <Tab name='learners' number='121k' />
     </div>
 
 
--- src/workshop/subheader/resources/Resources.js
+++ src/workshop/subheader/resources/Resources.js
@@ -1,10 +1,27 @@
 import React  from 'react'
 
 
-const Resources = props =>
-    <div>
-        Resources
+const Tab = ({name, number}) =>
+    <div className='workshop-filter-tab'>
+        <span className='workshop-filter-tab-number'>
+            {number}
+        </span>
+        <span className='workshop-filter-tab-name'>
+            {name}
+        </span>
     </div>
 
 
-export default Resources
+const Filter = props =>
+    <div className='workshop-filter'>
+        <Tab name='all' number='202k' />
+        <Tab name='video' number='151k' />
+        <Tab name='course' number='14' />
+        <Tab name='article' number='121k' />
+        <Tab name='book' number='121k' />
+        <Tab name='audio' number='121k' />
+        <Tab name='interactive' number='121k' />
+    </div>
+
+
+export default Filter
